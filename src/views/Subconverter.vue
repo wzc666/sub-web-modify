e4d9861b5424da3c160cc26b54f374d09d2041b6
Jason <wu452871156@gmail.com>
2024-12-31 14:41:31 +0800

    Update Subconverter.vue
    update custom backend

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -474,13 +474,15 @@ export default {
           "sub.cm": "https://sub.cm/short",
         },
         customBackend: {
-          "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
-          "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
-          "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
-          nameless13提供: "https://www.nameless13.com",
-          subconverter作者提供: "https://sub.xeton.dev",
-          "sub-web作者提供": "https://api.wcc.best",
-          "sub作者&lhie1提供": "https://api.dler.io",
+          "最强坤坤作者":"http://198.89.125.250:16677"
+          // "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
+          // "肥羊备用后端【vless reality+hy1+hy2】": "https://sub.d1.mk",
+          // "つつ-多地防失联【负载均衡+国内优化】": "https://api.tsutsu.one",
+          // nameless13提供: "https://www.nameless13.com",
+          // subconverter作者提供: "https://sub.xeton.dev",
+          // "sub-web作者提供": "https://api.wcc.best",
+          // "sub作者&lhie1提供": "https://api.dler.io",
+    
         },
         backendOptions: [
           {value: "https://url.v1.mk"},
